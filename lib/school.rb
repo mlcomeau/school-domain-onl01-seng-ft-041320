a1fe2f7decba7cae71e3048f254508d5f9d93cad
Megan Comeau <mlcomeau99@gmail.com>
2020-04-19 19:42:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,4 +6,26 @@ class School
     @roster = {}
   end
 
+  def add_student(student_name, grade)
+    @student_name = student_name
+    @grade = grade 
+
+    if !(@roster.include?(grade))
+      @roster[grade] = []
+    end
+
+    @roster[grade] << name 
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, student_name|
+      @roster[grade] = student_name.sort
+    end
+  end
+  
+
 end
